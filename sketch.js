a89e66bc0a8c93b0509dbbd9a9de64f35a514efa
Aimadkhan <90553916+Aimadkhan@users.noreply.github.com>
2022-03-26 00:40:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,8 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world, backgroundImg;
-var canvas, angle, tower;
+var canvas, angle, tower, ground, cannon;
+var balls = [];
 
 function preload() {
   backgroundImg = loadImage("./assets/background.gif");
@@ -15,12 +16,16 @@ function setup() {
   canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
-  angleMode(DEGREES)
-  angle = 15
- 
+  angleMode(DEGREES);
+  angle = 15;
+
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
+  World.add(world, ground);
+
   tower = Bodies.rectangle(160, 350, 160, 310, { isStatic: true });
   World.add(world, tower);
 
+  cannon = new Cannon(180, 110, 130, 100, angle);
 }
 
 function draw() {
@@ -29,26 +34,41 @@ function draw() {
 
   Engine.update(engine);
 
-
   
-
+  rect(ground.position.x, ground.position.y, width * 2, 1);
+ 
 
   push();
   imageMode(CENTER);
-  image(towerImage, tower.position.x, tower.position.y, 160, 310);
+  image(towerImage,tower.position.x, tower.position.y, 160, 310);
   pop();
 
+  for (var i = 0; i < balls.length; i++) {
+    showCannonBalls(balls[i]);
+  }
 
-
- 
-
-  
+  cannon.display();
 }
 
+function keyPressed() {
+  if (keyCode === DOWN_ARROW) {
+    var cannonBall = new CannonBall(cannon.x, cannon.y);
+    Matter.Body.setAngle(cannonBall.body, cannon.angle);
+    balls.push(cannonBall);
+  }
+}
 
-  
+function showCannonBalls(ball) {
+  if (ball) {
+    ball.display();
+  }
+}
 
-
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) {
+    balls[balls.length - 1].shoot();
+  }
+}
 
 
 
